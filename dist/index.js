0e79c9b5c0b671edaa629323f9e8fbca923ce40a
Bimochan Shrestha <bmochan@gmail.com>
2021-12-23 23:43:48 -0800

    Update build file

--- dist/index.js
+++ dist/index.js
@@ -8445,18 +8445,31 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(5197);
 const github = __nccwpck_require__(4510);
 
-try {
-	// `jira-project-url` input defined in action metadata file
-	const nameToGreet = core.getInput('jira-project-url');
-	console.log(`Hello ${nameToGreet}!`);
-	const time = new Date().toTimeString();
-	core.setOutput('time', time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
-} catch (error) {
-	core.setFailed(error.message);
+async function run() {
+	try {
+		const jirProjectUrl = core.getInput('jira-project-url');
+		const github_token = core.getInput('GITHUB_TOKEN');
+
+		const context = github.context;
+		if (context.payload.pull_request == null) {
+			core.setFailed('No pull request found.');
+			return;
+		}
+		const pull_request_number = context.payload.pull_request.number;
+
+		const octokit = new github.getOctokit(github_token);
+		const new_comment = octokit.rest.issues.createComment({
+			...context.repo,
+			issue_number: pull_request_number,
+			body: `Jira: ${jirProjectUrl}/JPT-123`,
+		});
+    console.log(new_comment)
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
+
+run();
 
 })();
 
