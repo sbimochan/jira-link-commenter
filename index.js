d9ea567decd309e7f09d696f821ed85f259a31a3
Bimochan Shrestha <bmochan@gmail.com>
2021-12-23 22:52:46 -0800

    PR commenter

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-	// `jira-project-url` input defined in action metadata file
-	const nameToGreet = core.getInput('jira-project-url');
-	console.log(`Hello ${nameToGreet}!`);
-	const time = new Date().toTimeString();
-	core.setOutput('time', time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
-} catch (error) {
-	core.setFailed(error.message);
+async function run() {
+	try {
+		const message = core.getInput('message');
+		const github_token = core.getInput('GITHUB_TOKEN');
+
+		const context = github.context;
+		if (context.payload.pull_request == null) {
+			core.setFailed('No pull request found.');
+			return;
+		}
+		const pull_request_number = context.payload.pull_request.number;
+
+		const octokit = new github.GitHub(github_token);
+		const new_comment = octokit.issues.createComment({
+			...context.repo,
+			issue_number: pull_request_number,
+			body: message,
+		});
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
+
+run();
