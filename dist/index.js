a9ec090a7f659e7864a3fcffb828aa321fe76316
Bimochan Shrestha <bmochan@gmail.com>
2021-12-23 23:49:23 -0800

    Add dist file

--- dist/index.js
+++ dist/index.js
@@ -8448,22 +8448,22 @@ const github = __nccwpck_require__(4510);
 async function run() {
 	try {
 		const jirProjectUrl = core.getInput('jira-project-url');
-		const github_token = core.getInput('GITHUB_TOKEN');
+		const githubToekn = core.getInput('GITHUB_TOKEN');
 
 		const context = github.context;
 		if (context.payload.pull_request == null) {
 			core.setFailed('No pull request found.');
+
 			return;
 		}
-		const pull_request_number = context.payload.pull_request.number;
-
-		const octokit = new github.getOctokit(github_token);
-		const new_comment = octokit.rest.issues.createComment({
+		const pullRequestNumber = context.payload.pull_request.number;
+    console.log(context.payload);
+		const octokit = new github.getOctokit(githubToekn);
+		await octokit.rest.issues.createComment({
 			...context.repo,
-			issue_number: pull_request_number,
+			issue_number: pullRequestNumber,
 			body: `Jira: ${jirProjectUrl}/JPT-123`,
 		});
-    console.log(new_comment)
 	} catch (error) {
 		core.setFailed(error.message);
 	}
